f534455ab006d537d1d8076702c22e64cbfed3dd
Dennis Kraffczyk <dennis.kraffczyk@gmail.com>
2022-04-02 23:09:44 +0200

    Add `constants.php` for autocompletion of constants

---
+++ constants.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Defines constants for autocompletion in IDEs. This file is not meant to be actively used anywhere!
+ *
+ * @author	Dennis Kraffczyk
+ * @copyright	2011-2022 KittMedia
+ * @license	Free <https://shop.kittmedia.com/core/licenses/#licenseFree>
+ * @package	com.kittmedia.wcf.faqExtendedSearch
+ * @category	Suite Core
+ */
+
+\define('EXTENDED_SEARCH_FAQ_COUNT', 10);
+\define('EXTENDED_SEARCH_FAQ_ENABLE_PATH', 1);
+\define('EXTENDED_SEARCH_FAQ_ENABLED', 1);
+
+// third party (de.cwalz.extendedSearch)
+\define('EXTENDED_SEARCH_SEARCH_TYPE', 'startend');
